41e340237ee0f70db1424c5f20e8bb53bce4ecef
Oscar Shoudt <shoudto@gmail.com>
2021-04-06 12:46:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function fetchBooks() {
-
+  return (
+    fetch("https://anapioficeandfire.com/api/books")
+    .then(response => response.json())
+    .then(json => renderBooks(json))
+  )
 }
 
 function renderBooks(json) {
